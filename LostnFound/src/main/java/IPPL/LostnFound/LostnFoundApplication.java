824288cd6c3f5f96506c1f0d109c96abc55ac113
valtheen <nauvalfatheen@gmail.com>
2025-10-15 20:41:45 +0700

    Initial commit

---
+++ LostnFound/src/main/java/IPPL/LostnFound/LostnFoundApplication.java
@@ -0,0 +1,13 @@
+package IPPL.LostnFound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostnFoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostnFoundApplication.class, args);
+	}
+
+}
